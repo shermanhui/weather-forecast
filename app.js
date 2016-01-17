c9cb01bcfc1979233395148ca5caaaa78d1c4deb
shui91 <sherman.sy.hui@gmail.com>
2016-01-17 13:51:05 -0800

    feat: retrieving weather JSON results

--- app.js
+++ app.js
@@ -17,12 +17,32 @@ myApp.config(function ($routeProvider) {
 	});
 });
 
+// services
+myApp.service('cityService', function(){
+	this.city = "Vancouver, BC";
+});
+
 // controllers
-myApp.controller('mainController', ['$scope', function($scope){
+myApp.controller('mainController', ['$scope', 'cityService', function($scope, cityService){
 
+	$scope.city = cityService.city;
+
+	$scope.$watch('city', function(){
+		cityService.city = $scope.city;
+	});
 }]);
 
-myApp.controller('weatherController', ['$scope', function($scope){
+myApp.controller('weatherController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
 
+	$scope.city = cityService.city;
+
+	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=ef9657c3d076894f78e9b2c86a213767', {
+		callback: "JSON_CALLBACK"
+	}, { get: { method: "JSONP"}});
+
+	$scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: 2});
+
+	console.log($scope.weatherResult);
 }]);
+
 
