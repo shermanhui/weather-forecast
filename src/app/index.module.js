6412dc8125837b9b94e83445ff6ff3f95368a9c4
shui91 <sherman.sy.hui@gmail.com>
2016-01-17 11:42:26 -0800

    feat: keep it simple, just html and js

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('weatherForecast', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'toastr']);
-
-})();
